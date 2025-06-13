9f85202fb39441899ff857c87f74dac01f4fcafa
Nithin Jayakumar <nithinjayakumar2205@gmail.com>
2025-06-13 10:56:28 +0530

    Added the change password functionality

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,6 +1,6 @@
 from rest_framework_simplejwt.views import TokenRefreshView,TokenObtainPairView
 from django.urls import path
-from .views import RegisterView,ProtectedHelloView,ForgotPassword,LogoutView
+from .views import RegisterView,ProtectedHelloView,ForgotPassword,LogoutView,ChangePassword
 
 urlpatterns = [
     path('register/',RegisterView.as_view(),name='register'),
@@ -9,4 +9,5 @@ urlpatterns = [
     path('login/refresh/',TokenRefreshView.as_view(),name='login_refresh'),
     path('forgot-password/',ForgotPassword.as_view(),name='forgot_password'),
     path('logout/',LogoutView.as_view(),name='logout'),
+    path('change-password/',ChangePassword.as_view(),name='change_password'),
 ]
--- accounts/views.py
+++ accounts/views.py
@@ -58,3 +58,31 @@ class LogoutView(APIView):
                 {'error':'Invalid or already used token'},
                 status=status.HTTP_400_BAD_REQUEST
             )
+
+class ChangePassword(APIView):
+    permission_classes = [IsAuthenticated]
+    def post(self, request):
+        user=request.user
+        old_password=request.data.get('old_password')
+        new_password=request.data.get('new_password')
+        if not old_password or not new_password:
+            return Response(
+                {'error':'Both old password and new password are required'},
+                status=status.HTTP_400_BAD_REQUEST
+            )
+        if old_password == new_password:
+            return Response(
+                {'error':'Old Password and New Password cannot be the same'},
+                status=status.HTTP_400_BAD_REQUEST
+            )
+        if not User.check_password(old_password):
+            return Response(
+                {'error':'Old password is incorrect'},
+                status=status.HTTP_403_FORBIDDEN
+            )
+        user.set_password(new_password)
+        user.save()
+        return Response(
+            {'message':'Succesfullt updated the password'},
+            status=status.HTTP_200_OK
+        )
